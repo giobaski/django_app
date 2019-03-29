4bb17d281d55519093bda4597f0ccfe007561c4f
giobaski <giobaski@gmail.com>
2019-03-29 17:19:14 +0400

    second upload

--- music/views.py
+++ music/views.py
@@ -1,7 +1,7 @@
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.http import Http404
 from .models import Album
-from django.shortcuts import render
 
 # Create your views here.
 def index(request):
@@ -9,10 +9,7 @@ def index(request):
     return render(request,'music/index.html',{'all_albums': all_albums})
 
 def detail(request, album_id):
-    try:
-        album = Album.objects.get(pk=album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album does not exist")
+    album = get_object_or_404(Album,pk=album_id)
     return render(request,'music/detail.html',{'album': album})
 
 
